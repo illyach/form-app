101f80a6703fd402b4dac77e11176c26989fb5f8
illyach <illyach@Illas-MacBook-Air.local>
2023-01-11 13:24:42 +0200

    practise form

--- src/App.js
+++ src/App.js
@@ -1,25 +1,74 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import {useForm} from "react-hook-form";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+export default function App() {
+
+    const {register, formState:{errors}, handleSubmit} = useForm()
+
+    const [field, setField] = useState()
+    const [submitted, setSubmit] = useState(false)
+
+    const onSubmit = (data) => {
+        setField(data)
+        setSubmit(true)
+    }
+
+    return(
+        <div className="form-container">
+            <form className="main-form" onSubmit={handleSubmit(onSubmit)}>
+
+                {submitted?<div className="success-msg">Registration succesfull!</div>:null}
+
+
+                <input
+                    id="first-name"
+                    placeholder="firstName"
+                    className="form-field"
+                    type="firstName"
+                    {...register('firstName', {required:"First name required succesful"})}
+                />
+
+                <span>{errors.firstName?.message}</span>
+
+
+                <input
+                    id="last-name"
+                    placeholder="Last Name"
+                    className="form-field"
+                    type="lastName"
+                    {...register('lastName', {required:"last name required succesful"})}
+                />
+
+                <span>{errors.lastName?.message}</span>
+
+                <input
+                    id="email-name"
+                    placeholder="email"
+                    className="form-field"
+                    type="email"
+                    {...register('email', {required:"email  required succesful"})}
+                />
+
+                <span>{errors.email?.message}</span>
+
+                <input
+                    id="password"
+                    placeholder="password"
+                    className="form-field"
+                    type="password"
+                    {...register('password', {required:"password  required succesful",
+                        minLength:{value:4, message:"Min length can be more than 4 characters"},
+                        maxLength:{value:20, message:"Max length can be less than 20 characters"},
+                    })}
+                />
+
+                <span>{errors.password?.message}</span>
+
+                <button type="submit">submit</button>
+            </form>
+        </div>
+    )
+
 }
-
-export default App;
